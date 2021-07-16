75fc0344750a3e17f5b2e11b00664ef6c4bdc445
juanf <juanignaciofernandez1234@gmail.com>
2021-07-16 18:21:17 -0300

    module 1

---
+++ playground/some.js
@@ -0,0 +1,5 @@
+console.log('Im in another file');
+
+const p = document.querySelector('p');
+
+console.log(p);
---
+++ playground/types.js
@@ -0,0 +1,48 @@
+/* eslint-disable*/
+/*
+const name = 'wes';
+const middle = "topher";
+const last = `bos`;
+
+const sentence = "she's so cool"
+
+const sentence2 = `she's so cool`;
+
+const hello = 'Hello my name is ' + name + '. Nice to meet you';
+
+const hello2 = `Hello my name is ${name}. Nice to meet you`;
+
+
+const html = `
+ <div>  
+ <h2>${name}</h2>
+ <p>${hello}</p>
+ </div>
+`
+document.body.innerHTML = html;
+
+
+const age = 100;
+const name = 'wes';
+
+//const a = 30;
+//const b = 20;
+
+const smarties = 20;
+const kids = 3;
+const each_kids_gets = Math.floor(smarties/kids);
+const dad_gets = smarties % kids;
+
+console.log(`There are ${smarties} smarties and ${kids} kids`);
+console.log(`Each kids gets ${each_kids_gets}`);
+console.log(`Dad gets ${dad_gets}`);
+
+*/
+const person ={
+    first: 'wes',
+    last: 'boss',
+    age: 100
+};
+
+let dog;
+console.log(dog);
